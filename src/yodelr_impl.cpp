508687a1801c6fdc4b6a9774639cf43183da3c77
George Fotopoulos <xorz57@gmail.com>
2024-09-20 17:16:49 +0300

    Implement addPost and extractTopics

--- src/yodelr_impl.cpp
+++ src/yodelr_impl.cpp
@@ -1,5 +1,7 @@
 #include <yodelr/yodelr_impl.h>
 
+#include <regex>
+
 using namespace yodelr;
 
 void YodelrImpl::addUser(const std::string &userName) {
@@ -7,7 +9,11 @@ void YodelrImpl::addUser(const std::string &userName) {
 }
 
 void YodelrImpl::addPost(const std::string &userName, const std::string &postText, std::uint64_t timestamp) {
-    // TODO
+    mUsersToTimestamps[userName].insert(timestamp);
+    mTimestampsToPosts[timestamp] = postText;
+    for (const std::string &topic: extractTopics(postText)) {
+        mTopicsToTimestamps[topic].insert(timestamp);
+    }
 }
 
 void YodelrImpl::deleteUser(const std::string &userName) {
@@ -50,4 +56,15 @@ PostTexts YodelrImpl::getPostsForTopic(const std::string &topic) const {
 Topics YodelrImpl::getTrendingTopics(std::uint64_t fromTimestamp, std::uint64_t toTimestamp) const {
     // TODO
     return Topics();
+}
+
+Topics YodelrImpl::extractTopics(const std::string &postText) {
+    Topics topics;
+    std::regex topicPattern(R"(\#(\w+))");
+    auto wordsBegin = std::sregex_iterator(postText.begin(), postText.end(), topicPattern);
+    auto wordsEnd = std::sregex_iterator();
+    for (std::sregex_iterator i = wordsBegin; i != wordsEnd; ++i) {
+        topics.push_back((*i)[1].str());
+    }
+    return topics;
 }
