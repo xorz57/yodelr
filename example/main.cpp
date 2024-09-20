e190fd1c21261cca52ee9770ac29d61ce5b72db1
George Fotopoulos <xorz57@gmail.com>
2024-09-20 11:39:35 +0300

    Refactor main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -14,14 +14,14 @@ int main() {
     service->addPost("john", "eating #steak for dinner", 2);
     service->addPost("john", "ugh! this #steak tasted like dog food", 3);
 
-    auto posts1 = service->getPostsForUser("john");
-    for (const auto &post: posts1) {
-        std::cout << post << "\n";
+    auto postsTexts1 = service->getPostsForUser("john");
+    for (const auto &postText: postsTexts1) {
+        std::cout << postText << "\n";
     }
 
-    auto posts2 = service->getPostsForTopic("steak");
-    for (const auto &post: posts2) {
-        std::cout << post << "\n";
+    auto postTexts2 = service->getPostsForTopic("steak");
+    for (const auto &postText: postTexts2) {
+        std::cout << postText << "\n";
     }
 
     auto topics1 = service->getTrendingTopics(1, 3);
