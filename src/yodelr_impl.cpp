810b1dfabf18bcd9e1517268717d793afe91a3ab
George Fotopoulos <xorz57@gmail.com>
2024-09-20 17:14:22 +0300

    Implement getPostsForUser

--- src/yodelr_impl.cpp
+++ src/yodelr_impl.cpp
@@ -20,8 +20,17 @@ void YodelrImpl::deleteUser(const std::string &userName) {
 }
 
 PostTexts YodelrImpl::getPostsForUser(const std::string &userName) const {
-    // TODO
-    return PostTexts();
+    PostTexts postTexts;
+    auto it1 = mUsersToTimestamps.find(userName);
+    if (it1 != mUsersToTimestamps.end()) {
+        for (auto timestamp: it1->second) {
+            auto it2 = mTimestampsToPosts.find(timestamp);
+            if (it2 != mTimestampsToPosts.end()) {
+                postTexts.push_back(it2->second);
+            }
+        }
+    }
+    return postTexts;
 }
 
 PostTexts YodelrImpl::getPostsForTopic(const std::string &topic) const {
